d6d6c550fac51fb4781bc7058be7697a9220e967
Rudra Pratap Singh <rudra@Rudras-MacBook-Air.local>
2024-02-19 01:24:10 +0530

    update script

--- build-server/script.js
+++ build-server/script.js
@@ -6,9 +6,7 @@ const mime = require('mime-types')
 const Redis = require('ioredis')
 require('dotenv').config()
 
-
-// const publisher = new Redis(process.env.REDIS_CONNECTION_URI)
-
+const publisher = new Redis(process.env.REDIS_CONNECTION_URI)
 
 const s3Client = new S3Client({
     region: process.env.AWS_REGION,
@@ -16,47 +14,46 @@ const s3Client = new S3Client({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
     },
-    endpoint: AWS_S3_ENDPOINT,
-    s3ForcePathStyle: true,
+    endpoint: process.env.AWS_S3_ENDPOINT,
+    // s3ForcePathStyle: true, // Uncomment if you are not using localstack
 })
 
 const PROJECT_ID = process.env.PROJECT_ID
 
-
-// function publishLog(log) {
-//     publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
-// }
+function publishLog(log) {
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
 
 async function init() {
     console.log('Executing script.js')
-    // publishLog('Build Started...')
+    publishLog('Build Started...')
     const outDirPath = path.join(__dirname, 'output')
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
     p.stdout.on('data', function (data) {
         console.log(data.toString())
-        // publishLog(data.toString())
+        publishLog(data.toString())
     })
 
     p.stdout.on('error', function (data) {
         console.log('Error', data.toString())
-        // publishLog(`error: ${data.toString()}`)
+        publishLog(`error: ${data.toString()}`)
     })
 
     p.on('close', async function () {
         console.log('Build Complete')
-        // publishLog(`Build Complete`)
+        publishLog(`Build Complete`)
         const distFolderPath = path.join(__dirname, 'output', 'dist')
         const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
 
-        // publishLog(`Starting to upload`)
+        publishLog(`Starting to upload`)
         for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file)
             if (fs.lstatSync(filePath).isDirectory()) continue;
 
             console.log('uploading', filePath)
-            // publishLog(`uploading ${file}`)
+            publishLog(`uploading ${file}`)
 
             const command = new PutObjectCommand({
                 Bucket: process.env.AWS_BUCKET,
@@ -66,10 +63,10 @@ async function init() {
             })
 
             await s3Client.send(command)
-            // publishLog(`uploaded ${file}`)
+            publishLog(`uploaded ${file}`)
             console.log('uploaded', filePath)
         }
-        // publishLog(`Done`)
+        publishLog(`Done`)
         console.log('Done...')
     })
 }
